703ee5cfd78efef82e201458710b021ec222cb92
Jurgen Schwietering <scjurgen@yahoo.com>
2012-05-01 12:07:15 +0300

    documentation update

--- AT42QT1070.h
+++ AT42QT1070.h
@@ -1,3 +1,10 @@
+/** Copyright Jurgen Schwietering
+ * This library contains functions for the AT42QT1070
+ * 
+ * with some modifications you should make it run for 
+ * AT42QT1060 (i2c address 0x12), see section 6.1 in the datasheet for the Address values
+
+ */
 
 #ifndef _AT42QT1070_h
 #define _AT42QT1070_h
@@ -6,8 +13,11 @@
 
 #include <Wire.h>
 
-// you can also install your own handler to trigger what ever you want.
-// but you should read registers 2 and 3 soon after
+#define POLLING_USED 1 //!< use this only if you are going to poll (it will need  a bit of sram memory)
+
+/** you can also install your own handler to trigger what ever you want.
+ but you should read registers 2 and 3 soon after
+*/
 extern "C" void AT42QT_CHANGEISR(void) __attribute__ ((signal));
 
 #define AT42QT1070_MAXKEYS 7
@@ -18,14 +28,14 @@ class AT42QT1070
 public: // some public definitions first
 	typedef enum
 	{
-		CHIPID = 0, // should be 0x2E
-		FIRMWARE= 1,  // could be 0x15 ;)
-		DETECTIONSTATUS=2, //Bit7=calibrate, Bit6=Overflow, Bit0=Touch
-		KEYSTATUS=3, // Bit=Key
-		KEYSIGNAL=4, // MSB,LSB 
-		REFERENCE=18,//MSB,LSB
+		CHIPID = 0, //!< should be 0x2E
+		FIRMWARE= 1,  //!< could be 0x15 ;)
+		DETECTIONSTATUS=2, //!<Bit7=calibrate, Bit6=Overflow, Bit0=Touch
+		KEYSTATUS=3, //!< Bit=Key
+		KEYSIGNAL=4, //!< MSB,LSB of signal 
+		REFERENCE=18,//!<MSB,LSB of reference value (after calibration these change)
 		// subsequent are R/W
-		NEGTHRESHOLD=32, // don't set 0, default 20
+		NEGTHRESHOLD=32, //!< don't set 0, default 20
 		AVERAGE_ADJACENTKEYSUPPRESSIONLEVEl=39, 
 			//  BIT7-BIT2 average of samples (allowed 1,2,4,8,16,32) default 8 
 			// BIT2-BIT0=Define AKS group (0..3)
@@ -43,28 +53,28 @@ public: // some public definitions first
 	typedef void (*ErrorCallback)(const uint8_t);
 
 private:
-	static const uint8_t AT42I2CAdr=0x1B;
-	//void AT42QT_CHANGEISR(void); 
+    static const uint8_t AT42I2CAdr=0x1B; //!< this is constant for this chip
 	
-	ErrorCallback _errorCallback;
-	uint8_t checkResult(uint8_t result);
-	uint16_t refSet[AT42QT1070_MAXKEYS]; // internal copy of ref set
+    ErrorCallback _errorCallback;
+    uint8_t checkResult(uint8_t result);
+    uint8_t setActiveAddress(uint8_t adr);
+
 public: 
-	static AT42QT1070 *mySingleSelf;
-	volatile uint8_t AT42QTchangeEvent;
+    static AT42QT1070 *mySingleSelf;
+    volatile uint8_t AT42QTchangeEvent;
  
 public:
     AT42QT1070();
     
-    void setInternalISR(uint8_t pin); // pin 2 or 3 of arduino
+    void setInternalISR(uint8_t pin); //!< pin 2 or 3 of arduino, sets the internal interrupt handler, you can set your own one if you need, the qt1070 does alwaus pull LOW the CHANGE line.
     
-    void setErrorCallback(void (*cb)(uint8_t));
+    void setErrorCallback(void (*cb)(uint8_t)); //!< you can set a callback function if I2C fails to work
     
     void reset(void);
     void calibrate(void);
     uint8_t isCalibrating(void);
     
-	uint16_t readKeyValues(uint8_t key);
+    uint16_t readKeyValues(uint8_t key);
    
     void readKeySet();
 
@@ -74,8 +84,18 @@ public:
     void lowPowerMode(uint8_t val);
     void maxOnDuration(uint8_t val);
     
-    void completeDiffSet(int8_t *delta);
-    void changedDiffSet(int8_t *delta);
+    /**! the next set of functions is for polling operations,
+    these are only needed if you require more control on the buttons (multiple press at same time i.e.)
+    they are intensive in means of cpu time
+    after a calibration the refSet needs to be refreshed using completeDiffSet, if you fail to do that the changedDiffSet
+    could be off.
+    To understand that a calibration has been done you need to read the DETECTIONSTATUS registry or use the wrapper isCalibrating()
+    */
+#if POLLINGUSED
+    uint16_t refSet[AT42QT1070_MAXKEYS]; //!< internal copy of ref set only used if using POLLING mode
+    void completeDiffSet(int8_t *delta); //!< get difference of keyvalues delta[i]=actual[i]-reference[i]
+    void changedDiffSet(int8_t *delta); //!< as completeDiffSet but suppose that reference did not change so it is not loaded
+#endif
 
 };
 
